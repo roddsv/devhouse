e2b86b7090d5ec93d026a9a9f21aed6c5fc91485
Rodrigo Soares <adsrodsoares@gmail.com>
2025-01-18 04:40:49 -0300

    Melhorando validações com yup

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,9 +1,20 @@
 import User from '../models/User';
+import * as Yup from 'yup';
 
 class SessionController {
     async store(req, res) {
 
         const { email } = req.body;
+
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required()
+        })
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({
+                error: 'Falha na validação.'
+            })
+        }
 
         let user = await User.findOne({ email });
 
